7d5c1f920d01a0f6946182e17293b69a5207ce95
abhishekgpta <gptaabhishek93@gmail.com>
2018-10-15 22:20:03 +0530

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,60 @@
 var socket = io();
 
-	socket.on("connect",function(){
-		console.log("connected to server")
+function scrollToBottom(){
+	var messages = jQuery("#messages");
+	var newMessage = messages.children('li:last-child');
+	//heights
+	var clientHeight = messages.prop("clientHeight");
+	var scrollTop = messages.prop("scrollTop");
+	var scrollHeight = messages.prop("scrollHeight");
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
 
-		// socket.emit('createMessage',{
-		// 	from:'abhishek@emapl.com',
-		// 	text:"whatsup man"
-		// });
-	});
-	socket.on("disconnect",function(){
-		console.log("disconnected from server")
-	});
+	if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight)
+	}
+}	
+socket.on("connect",function(){
+	console.log("connected to server")
 
-	socket.on("newMessage",function(message){
-		// console.log("new message",message);
-		var formattedTime = moment(message.createdAt).format('h:mm a')
-		var template = jQuery("#message-template").html();
-		var html = Mustache.render(template,{
-			text:message.text,
-			from:message.from,
-			createdAt:formattedTime
-		});
-		jQuery("#messages").append(html)
-	});
-	socket.on("newLocationMessage",function(message){
-		var formattedTime = moment(message.createdAt).format('h:mm a');
-		var template = jQuery("#location-message-template").html();
-		var html = Mustache.render(template,{
-			from:message.from,
-			createdAt:formattedTime,
-			url:message.url,
-		});
-		jQuery("#messages").append(html);
-	});
 	// socket.emit('createMessage',{
-	// 	from:'abhishek',
-	// 	text:"hi"
-	// },function(data){
-	// 	console.log("got it",data);
+	// 	from:'abhishek@emapl.com',
+	// 	text:"whatsup man"
 	// });
+});
+socket.on("disconnect",function(){
+	console.log("disconnected from server")
+});
+
+socket.on("newMessage",function(message){
+	// console.log("new message",message);
+	var formattedTime = moment(message.createdAt).format('h:mm a')
+	var template = jQuery("#message-template").html();
+	var html = Mustache.render(template,{
+		text:message.text,
+		from:message.from,
+		createdAt:formattedTime
+	});
+	jQuery("#messages").append(html);
+	scrollToBottom();
+});
+socket.on("newLocationMessage",function(message){
+	var formattedTime = moment(message.createdAt).format('h:mm a');
+	var template = jQuery("#location-message-template").html();
+	var html = Mustache.render(template,{
+		from:message.from,
+		createdAt:formattedTime,
+		url:message.url,
+	});
+	jQuery("#messages").append(html);
+	scrollToBottom();
+});
+// socket.emit('createMessage',{
+// 	from:'abhishek',
+// 	text:"hi"
+// },function(data){
+// 	console.log("got it",data);
+// });
 jQuery("#message-form").on("submit",function(e){
 	e.preventDefault();
 
